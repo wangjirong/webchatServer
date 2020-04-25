3810f07314115f7ac4bf4f10fcb31d47c5ea09d5
wangjirong <408718358@qq.com>
2020-04-25 18:49:10 +0800

    websocket com some

--- bin/websocket.js
+++ bin/websocket.js
@@ -1,9 +1,8 @@
 const io = require('./www')
-console.log(io)
-io.on('connection',(socket)=>{
-    console.log('a user connected')
-    socket.on('login',(msg)=>{
-        console.log(msg);
-        socket.emit('relogin',msg);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('login',msg=>{
+        console.log(msg)
+        socket.emit("gun","给爷爬");
     })
-})
+});
