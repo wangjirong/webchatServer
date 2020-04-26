d21cf2061c6343c07992247362e27212dd5049d9
wangjirong <408718358@qq.com>
2020-04-26 19:35:52 +0800

    ..

--- model/Message.js
+++ model/Message.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
+
+
 module.exports = Message = mongoose.model("Message", mongoose.Schema({
-    user_id: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        required: true
+    /**
+     * @每两个人之间对应一条数据-数据保存到数组里-若没有这条数据则新增
+     * @findOneAndUpdate
+     * */
+    user: {
+        type: Object,
+        required: true,
     },
     receiver: {
-        type: String,
+        type: Object,
         required: true
     },
-    message: {
-        type: String,
-        required: true
+    messages: {
+        type: Array,
+        default: []
     },
-    time: {
-        type: Date,
-        default: new Date(),
-    }
 }));
--- utils/message.js
+++ utils/message.js
@@ -1,1 +1,25 @@
 const Message = require('../model/Message');
+
+class Mess {
+    constructor(text, time) {
+        this.text = text;
+        this.time = time;
+    }
+}
+
+
+function addMessage(user, receiver, message) {
+    return new Promise((resolve, reject) => {
+        Message.findOneAndUpdate({user, receiver}, {
+            '$push': {
+                message: new Mess(message, new Date())
+            }
+        }).then(res => {
+            resolve(res);
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
+
+module.exports = {addMessage};
